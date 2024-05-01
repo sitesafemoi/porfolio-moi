0a8b41bd5d39143ed99ddd3da3fca4cf950a5983
Moi <moisdan777@gmail.com>
2024-05-01 12:48:59 -0500

    intento del toggle

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// const toggle = document.getElementById(`switch`)
+
+// toggle.onchange = () => {
+//     const mode = this.checked ? "light" : "dark"
+//     document.documentElement.style.setProperty(`color-scheme`, mode)
+// }
+
+// const backGrund = document.querySelector(`.BackGround`)
+
+// var lightMode
+
+//  toggle.addEventListener(`change`, () => {
+    
+//      if(lightMode){
+//          backGrund.classList.remove(`white-background`) 
+//          backGrund.classList.add(`black-background`)
+         
+//      }else{
+//         backGrund.classList.remove(`black-background`) 
+//         backGrund.classList.add(`white-background`)
+//         return lightMode = false;
+//      }
+
+// })
