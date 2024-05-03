74ca73d3f7e802e2037f5fd961b709b38ffc4480
Moi <moisdan777@gmail.com>
2024-05-02 21:45:38 -0500

    Adding scripts that detected the light mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,25 @@
 const toggle = document.getElementById(`switch`)
 const backGrund = document.querySelector(`.BackGround`)
+const estado = window.matchMedia((`(prefers-color-scheme: light)`))
+
+//Detectar prefers-theme y poner el light mode o dejar el dark
+if(estado.matches){
+    document.body.classList.add(`light-theme`)
+    backGrund.classList.remove(`black-background`)
+    backGrund.classList.add(`white-background`)
+}
 
 toggle.addEventListener(`change`, () => {
     
-        document.body.classList.toggle(`light-theme`)
-        backGrund.classList.toggle(`black-background`)
-        backGrund.classList.toggle(`white-background`)
-   
+         document.body.classList.toggle(`light-theme`)
+         backGrund.classList.toggle(`black-background`)
+         backGrund.classList.toggle(`white-background`)
+
+        if(estado.matches){
+            backGrund.classList.add(`black-background-dark`)
+            backGrund.classList.remove(`black-background`)
+            document.body.classList.toggle(`dark-theme`)
+            return
+        }
+
 })
